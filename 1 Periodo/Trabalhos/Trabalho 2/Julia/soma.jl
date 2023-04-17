8e70c6271673478a9724765c671a016d90988f19
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-04-17 13:11:41 -0300

    Update soma.jl

--- 1 Periodo/Trabalhos/Trabalho 2/Julia/soma.jl
+++ 1 Periodo/Trabalhos/Trabalho 2/Julia/soma.jl
@@ -4,12 +4,14 @@ Linguagem: Julia
 =#
 
 println("Escolha dois valores para serem somados:")
+
 println("Valor 1:")
-numero1 = readline()
-numero1 = parse(Int64, numero1)
+numero1 = readline()#leitura da string
+numero1 = parse(Float64, numero1)#converção de string para float
+
 println("Valor 2:")
 numero2 = readline()
-numero2 = parse(Int64, numero2)
+numero2 = parse(Float64, numero2)
 
 soma = numero1 + numero2
 
