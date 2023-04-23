c16b61b281030e8748ba1e217446ee58810299a4
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-04-23 15:42:08 -0300

    Update soma.jl

--- 1 Periodo/Laboratorios/Laboratorio 2/Funcional/soma.jl
+++ 1 Periodo/Laboratorios/Laboratorio 2/Funcional/soma.jl
@@ -4,12 +4,15 @@ Paradigma = Funcional
 •SomaNumero - Programa que lê dois números do usuário e imprima a soma desses números na tela.
 =#
 
+function soma(valor1, valor2)
+    return valor1 + valor2
+end
+
 println("Escolha um valor para o 1º número: ")
 numero1 = parse(Float64, readline())#leitura da string com readline(), e converção para float com parse()
 
 println("Escolha um valor para o 2º número: ")
 numero2 = parse(Float64, readline())
 
-soma = numero1 + numero2
 
-println("O valor da soma entre $numero1 e $numero2 resulta em $soma")
+println("O valor da soma entre $numero1 e $numero2 resulta em ", soma(numero1, numero2))
