ae068f68929c7df82e8083af649c8174e9620592
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-04-23 16:19:36 -0300

    POO

--- 1 Periodo/Laboratorios/Laboratorio 2/Orientada a Objetos/soma.rb
+++ 1 Periodo/Laboratorios/Laboratorio 2/Orientada a Objetos/soma.rb
@@ -4,12 +4,16 @@ Paradigma = Orientada a Objetos (POO)
 •SomaNumero - Programa que lê dois números do usuário e imprima a soma desses números na tela.
 =end
 
+class Soma
+  def self.soma(valor1, valor2)
+    valor1 + valor2
+  end
+end
+
 puts "Escolha um valor para o 1º número: "
-numero1 = gets.to_f#leitura da string com o gets, e conversão para float com o to_f
+numero1 = gets.chomp.to_f#leitura da string com o gets, e conversão para float com o to_f
 
 puts "Escolha um valor para o 2º número: "
-numero2 = gets.to_f
+numero2 = gets.chomp.to_f
 
-soma = numero1 + numero2
-
-puts "O valor da soma entre #{numero1} e #{numero2} resulta em #{soma}"
+puts "O valor da soma entre #{numero1} e #{numero2} resulta em #{Soma.soma(numero1, numero2)}"
